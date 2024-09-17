27e8ec9720e3763c35d9c38b2a0caabe918fd0fa
Prem <31947782+PremYekkali@users.noreply.github.com>
2024-09-17 20:56:19 +0530

    Add files via upload
    1) Added faucet for minting stakeToken
    2) Approving if allowance is lesser than stakeamount

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Set up Web3 and the smart contract
 let web3;
 let stakingContract;
+let currentAccount;
 const stakingContractAddress = '0x1231DF9091BcBf76cd227a1031E362B7E350bBB8';
 const stakingContractABI = [
 	{
@@ -326,10 +327,404 @@ const stakingContractABI = [
 	}
 ]
 
+const tokenABI = [
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "approve",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "burn",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "burnFrom",
+		"outputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "_newOperator",
+				"type": "address"
+			}
+		],
+		"name": "changeOperator",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"name": "subtractedValue",
+				"type": "uint256"
+			}
+		],
+		"name": "decreaseAllowance",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"name": "addedValue",
+				"type": "uint256"
+			}
+		],
+		"name": "increaseAllowance",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "account",
+				"type": "address"
+			},
+			{
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "mint",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "transfer",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "transferFrom",
+		"outputs": [
+			{
+				"name": "",
+				"type": "bool"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"name": "_initialSupply",
+				"type": "uint256"
+			},
+			{
+				"name": "_initialTokenHolder",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "from",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"name": "to",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Transfer",
+		"type": "event"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": true,
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"indexed": true,
+				"name": "spender",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "value",
+				"type": "uint256"
+			}
+		],
+		"name": "Approval",
+		"type": "event"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "owner",
+				"type": "address"
+			},
+			{
+				"name": "spender",
+				"type": "address"
+			}
+		],
+		"name": "allowance",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [
+			{
+				"name": "account",
+				"type": "address"
+			}
+		],
+		"name": "balanceOf",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "decimals",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint8"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "name",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "operator",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "symbol",
+		"outputs": [
+			{
+				"name": "",
+				"type": "string"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "totalSupply",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+]
+const tokenAddress = '0xa95d8e49d072b108Ac12D03DFFdF8C941A6f25b3';
+
+let tokenContract;
+
+async function getAllowance(owner, spender) {
+    return await tokenContract.methods.allowance(owner, spender).call();
+}
+
+async function approveTokens(spender, amount) {
+    return new Promise((resolve, reject) => {
+        tokenContract.methods.approve(spender, amount).send({ from: currentAccount })
+        .on('transactionHash', function(hash) {
+            console.log('Transaction hash:', hash);
+        })
+        .on('receipt', function(receipt) {
+            console.log('Transaction receipt:', receipt);
+            resolve(receipt);
+        })
+        .on('error', function(error) {
+            console.error('Error approving tokens:', error);
+            reject(error);
+        });
+    });
+}
+
 window.addEventListener('load', async () => {
     if (window.ethereum) {
         web3 = new Web3(window.ethereum);
         await window.ethereum.request({ method: 'eth_requestAccounts' });
+        const accounts = await web3.eth.getAccounts();
+        currentAccount = accounts[0];
+        console.log("address:   ",currentAccount);
         initContract();
         loadStakingEvents(); // Optionally, load events on page load
     } else {
@@ -339,6 +734,7 @@ window.addEventListener('load', async () => {
 
 function initContract() {
     stakingContract = new web3.eth.Contract(stakingContractABI, stakingContractAddress);
+    tokenContract = new web3.eth.Contract(tokenABI, tokenAddress);
 }
 
 async function loadStakingEvents() {
@@ -392,18 +788,32 @@ async function loadStakingEvents() {
     .on('error', console.error);
 }
 
-async function stakeTokens() {
-    const amount = document.getElementById('stake-amount').value;
-    const accounts = await web3.eth.getAccounts();
-    const account = accounts[0];
-
+async function stakeTokens(amount) {
     try {
-        await stakingContract.methods.stake(web3.utils.toWei(amount, 'ether')).send({ from: account });
-        alert('Tokens staked successfully!');
-        loadStakingEvents();  // Refresh events
+        const allowance = await getAllowance(currentAccount, stakingContractAddress);
+        const amountInWei = web3.utils.toWei(amount, 'ether');
+
+        if (web3.utils.toBN(allowance).lt(web3.utils.toBN(amountInWei))) {
+            console.log('Allowance is insufficient. Requesting approval...');
+            showLoader();
+            await approveTokens(stakingContractAddress, amountInWei);
+            hideLoader();
+        }
+
+        // Proceed with staking
+        stakingContract.methods.stake(amountInWei).send({ from: currentAccount })
+        .on('transactionHash', function(hash) {
+            console.log('Staking transaction hash:', hash);
+        })
+        .on('receipt', function(receipt) {
+            console.log('Staking transaction receipt:', receipt);
+            loadStakingEvents(); // Refresh events after staking
+        })
+        .on('error', function(error) {
+            console.error('Error staking tokens:', error);
+        });
     } catch (error) {
-        console.error(error);
-        alert('Error staking tokens.');
+        console.error('Error in staking process:', error);
     }
 }
 
@@ -433,5 +843,43 @@ async function withdrawInterest() {
     } catch (error) {
         console.error(error);
         alert('Error withdrawing interest.');
+    }
+}
+
+const loader = document.getElementById('loader'); // Get the loader element
+
+function showLoader() {
+    loader.style.display = 'block'; // Show the loader
+}
+
+function hideLoader() {
+    loader.style.display = 'none'; // Hide the loader
+}
+
+async function mintStakeTokens() {
+    if (!window.ethereum) {
+        alert('Please install MetaMask!');
+        return;
+    }
+
+    const accounts = await web3.eth.requestAccounts();
+    const currentAccount = accounts[0];
+    const amountToMint = document.getElementById('mint-amount').value;
+
+    if (!amountToMint || isNaN(amountToMint) || amountToMint <= 0) {
+        alert('Please enter a valid amount to mint.');
+        return;
+    }
+
+    // Convert amount to the appropriate units
+    const amountInWei = web3.utils.toWei(amountToMint, 'ether');
+
+    try {
+        // Call the mint function on the contract
+        await tokenContract.methods.mint(currentAccount, amountInWei).send({ from: currentAccount });
+        alert('Tokens minted successfully!');
+    } catch (error) {
+        console.error('Error minting tokens:', error);
+        alert('Error minting tokens. Check console for details.');
     }
 }
